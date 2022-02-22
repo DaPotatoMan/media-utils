590c3bfb8e549789c28683be224ddcecbdfdbeb9
Fahadul Islam <44726129+DaPotatoMan@users.noreply.github.com>
2022-02-22 16:49:05 +0600

    feat: Improve watchPermission destoy function

--- src/permissions/index.ts
+++ src/permissions/index.ts
@@ -30,19 +30,22 @@ export function requestPermission(name: MediaPermission) {
  * @param name Name of the permission to watch
  * @param onChange Callback function that is triggered when permission is changed
  * @param immediate Invoke `onChange` immediately on initialization
+ * @returns Function that destroys the watcher when invoked
  */
 export function watchPermission(name: MediaPermission, onChange: (status: PermissionStatus) => void, immediate?: boolean) {
-  return navigator.permissions
+  const destroy = navigator.permissions
     .query({ name: name as PermissionName })
     .then((status) => {
       if (immediate) onChange(status)
       status.addEventListener('change', () => onChange(status))
 
-      /** Destroy permission watcher */
-      return () => {
-        status.removeEventListener('change', () => onChange(status))
-      }
+      return () => status.removeEventListener('change', () => onChange(status))
     })
+
+  /** Destroy permission watcher */
+  return () => {
+    destroy.then(fn => fn?.())
+  }
 }
 
 /**
@@ -57,7 +60,7 @@ export function watchPermissions(
   onChange: (name: MediaPermission, status: PermissionStatus) => void,
   immediate?: boolean
 ) {
-  const destroyFns: Promise<VoidFunction>[] = []
+  const destroyFns: VoidFunction[] = []
 
   names.forEach(name =>
     destroyFns.push(
@@ -66,11 +69,7 @@ export function watchPermissions(
   )
 
   /** Destroy watcher */
-  return () => {
-    Promise.all(destroyFns).then(fns =>
-      fns.forEach(fn => fn?.())
-    )
-  }
+  return () => destroyFns.forEach(fn => fn?.())
 }
 
 /** Check if permission has been given for a media (camera/microphone) */
